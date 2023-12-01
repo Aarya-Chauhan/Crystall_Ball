6c51b4f951c2be5258133bd547a42b87713cd12e
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-12-01 21:20:45 +0530

    Added updated files

---
+++ app.py
@@ -0,0 +1,110 @@
+import streamlit as st
+from operator import index
+import plotly.express as px
+from pycaret.classification import setup, compare_models, pull, save_model, load_model
+import ydata_profiling
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
+import os
+from streamlit_option_menu import option_menu
+
+
+st.set_page_config(
+    page_title="Crystal Ball",
+    page_icon="🔮",
+    layout="wide",
+
+)
+
+
+
+# Nav Bar
+selected = option_menu(
+    menu_title="Project Crystal Ball",
+    options=["Dashboard", "Classification", "Regression", "Sample Applications"],
+    icons=["boxes", "layout-wtf", "graph-up-arrow","grid-fill"],
+    menu_icon="stack",
+    orientation="horizontal")
+
+if selected == "Dashboard":
+    pass
+
+# Classification Trainer Code
+
+if selected == "Classification":
+    with st.sidebar:
+        st.image("Core.gif")
+        st.title("Crystal Ball : Classification Trainer")
+        choice = st.radio(
+            "Workflow 👇", ["Upload", "Profiling", "Modelling", "Download"])
+        st.info("This is an AutoML app for Classification problems just upload a dataset and go through the selection steps only this time let our app do all the hardwork.")
+
+    if choice == "Upload":
+        st.title("Upload Your Dataset")
+        file = st.file_uploader("Upload Your Dataset")
+        if file:
+            df = pd.read_csv(file, index_col=None)
+            df.to_csv('dataset.csv', index=None)
+            st.dataframe(df)
+
+    if choice == "Profiling":
+        st.title("Exploratory Data Analysis")
+        profile_df = df.profile_report()
+        st_profile_report(profile_df)
+
+    if choice == "Modelling":
+        chosen_target = st.selectbox('Choose the Target Column', df.columns)
+        if st.button('Run Modelling'):
+            setup(df, target=chosen_target)
+            setup_df = pull()
+            st.dataframe(setup_df)
+            best_model = compare_models()
+            compare_df = pull()
+            st.dataframe(compare_df)
+            save_model(best_model, 'best_model')
+
+    if choice == "Download":
+        with open('best_model.pkl', 'rb') as f:
+            st.download_button('Download Model', f, file_name="best_model.pkl")
+
+# Regression Trainer Code
+
+if selected == "Regression":
+    with st.sidebar:
+        st.image("Core.gif")
+        st.title("Crystal Ball : Regression Trainer")
+        choice = st.radio(
+            "Workflow 👇", ["Upload", "Profiling", "Modelling", "Download"])
+        st.info("This is an AutoML app for Regression problems just upload a dataset and go through the selection steps only this time let our app do all the hardwork.")
+
+    if choice == "Upload":
+        st.title("Upload Your Dataset")
+        file = st.file_uploader("Upload Your Dataset")
+        if file:
+            df = pd.read_csv(file, index_col=None)
+            df.to_csv('dataset.csv', index=None)
+            st.dataframe(df)
+
+    if choice == "Profiling":
+        st.title("Exploratory Data Analysis")
+        profile_df = df.profile_report()
+        st_profile_report(profile_df)
+
+    if choice == "Modelling":
+        chosen_target = st.selectbox('Choose the Target Column', df.columns)
+        if st.button('Run Modelling'):
+            setup(df, target=chosen_target)
+            setup_df = pull()
+            st.dataframe(setup_df)
+            best_model = compare_models()
+            compare_df = pull()
+            st.dataframe(compare_df)
+            save_model(best_model, 'best_model')
+
+    if choice == "Download":
+        with open('best_model.pkl', 'rb') as f:
+            st.download_button('Download Model', f, file_name="best_model.pkl")
+
+
+if selected == "Sample Applications":
+    pass
